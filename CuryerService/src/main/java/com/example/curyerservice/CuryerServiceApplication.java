474096258f129be6b3c2596086ef7c395befdf4b
AzamatJurayev <aaab102600@gmail.com>
2022-08-06 11:42:11 +0300

    curyer Qo'shdim

---
+++ CuryerService/src/main/java/com/example/curyerservice/CuryerServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.curyerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuryerServiceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CuryerServiceApplication.class, args);
+        System.out.println("lsdklksdmvd");
+    }
+
+}
---
+++ CuryerService/src/test/java/com/example/curyerservice/CuryerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curyerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuryerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
